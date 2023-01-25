ee16c3176aabb4b8074bc33064cd1eac8d1b6c8b
Kehinde Malagu <Kehindemalagu@gmail.com>
2023-01-25 08:04:24 +0100

    Initial Setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        regular: "MontrealRegular,sans-serif",
+        book: "MontrealBook,sans-serif",
+        mthin: "MontrealThin,sans-serif",
+      },
+      colors : {
+        
+      }
+    },
+  },
+  plugins: [],
+}
